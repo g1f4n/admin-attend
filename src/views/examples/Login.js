52a758cbfa38d0442736f07c4c17474d65d4b92d
pieroashady <aldipiero11@gmail.com>
2020-09-17 15:44:56 +0700

    change password to md5

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -19,70 +19,70 @@ import React from 'react';
 
 // reactstrap components
 import {
-	Button,
-	Card,
-	CardHeader,
-	UncontrolledAlert,
-	CardBody,
-	FormGroup,
-	Form,
-	Input,
-	InputGroupAddon,
-	InputGroupText,
-	InputGroup,
-	Row,
-	Spinner,
-	Col
+  Button,
+  Card,
+  CardHeader,
+  UncontrolledAlert,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Spinner,
+  Col
 } from 'reactstrap';
 
 import Parse from 'parse';
+import md5 from 'md5';
 
 class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			loading: false,
-			username: '',
-			password: '',
-			error: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      username: '',
+      password: '',
+      error: ''
+    };
+  }
 
-	componentDidMount() {
-		console.log('Mounted');
-	}
+  componentDidMount() {
+    console.log('Mounted');
+  }
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.setState({ loading: true });
-		const { username, password } = this.state;
-		Parse.User
-			.logIn(username, password)
-			.then((x) => {
-				if (x.get('roles') !== 'admin') {
-					Parse.User.logOut();
-					alert('Selain admin dilarang masuk');
-					window.location.reload();
-					return;
-				}
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const { username, password } = this.state;
+    Parse.User.logIn(username, md5(password))
+      .then((x) => {
+        if (x.get('roles') !== 'admin') {
+          Parse.User.logOut();
+          alert('Selain admin dilarang masuk');
+          window.location.reload();
+          return;
+        }
 
-				this.setState({ loading: false });
-				this.props.history.push('/admin/index');
-			})
-			.catch((err) => {
-				console.log(err);
-				this.setState({ error: err.message, loading: false });
-			});
-	};
+        this.setState({ loading: false });
+        this.props.history.push('/admin/index');
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ error: err.message, loading: false });
+      });
+  };
 
-	render() {
-		const { loading, error } = this.state;
+  render() {
+    const { loading, error } = this.state;
 
-		return (
-			<React.Fragment>
-				<Col lg="5" md="7">
-					<Card className="bg-secondary shadow border-0">
-						{/* <CardHeader className="bg-transparent pb-5">
+    return (
+      <React.Fragment>
+        <Col lg="5" md="7">
+          <Card className="bg-secondary shadow border-0">
+            {/* <CardHeader className="bg-transparent pb-5">
               <div className="text-muted text-center mt-2 mb-3">
                 <small>Sign in with</small>
               </div>
@@ -117,52 +117,50 @@ class Login extends React.Component {
                 </Button>
               </div>
             </CardHeader> */}
-						<CardBody className="px-lg-5 py-lg-5">
-							<div className="text-center text-muted mb-4">
-								{error !== '' ? (
-									<UncontrolledAlert color="danger" fade={false}>
-										<span className="alert-inner--text">{error}</span>
-									</UncontrolledAlert>
-								) : (
-									<small>Masukkan username dan password</small>
-								)}
-							</div>
-							<Form role="form" onSubmit={this.handleSubmit}>
-								<FormGroup className="mb-3">
-									<InputGroup className="input-group-alternative">
-										<InputGroupAddon addonType="prepend">
-											<InputGroupText>
-												<i className="ni ni-circle-08" />
-											</InputGroupText>
-										</InputGroupAddon>
-										<Input
-											placeholder="Username"
-											type="text"
-											autoComplete="new-email"
-											required={true}
-											onChange={(e) =>
-												this.setState({ username: e.target.value })}
-										/>
-									</InputGroup>
-								</FormGroup>
-								<FormGroup>
-									<InputGroup className="input-group-alternative">
-										<InputGroupAddon addonType="prepend">
-											<InputGroupText>
-												<i className="ni ni-lock-circle-open" />
-											</InputGroupText>
-										</InputGroupAddon>
-										<Input
-											placeholder="Password"
-											type="password"
-											autoComplete="new-password"
-											required={true}
-											onChange={(e) =>
-												this.setState({ password: e.target.value })}
-										/>
-									</InputGroup>
-								</FormGroup>
-								{/* <div className="custom-control custom-control-alternative custom-checkbox">
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                {error !== '' ? (
+                  <UncontrolledAlert color="danger" fade={false}>
+                    <span className="alert-inner--text">{error}</span>
+                  </UncontrolledAlert>
+                ) : (
+                  <small>Masukkan username dan password</small>
+                )}
+              </div>
+              <Form role="form" onSubmit={this.handleSubmit}>
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-circle-08" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      placeholder="Username"
+                      type="text"
+                      autoComplete="new-email"
+                      required={true}
+                      onChange={(e) => this.setState({ username: e.target.value })}
+                    />
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      placeholder="Password"
+                      type="password"
+                      autoComplete="new-password"
+                      required={true}
+                      onChange={(e) => this.setState({ password: e.target.value })}
+                    />
+                  </InputGroup>
+                </FormGroup>
+                {/* <div className="custom-control custom-control-alternative custom-checkbox">
                   <input
                     className="custom-control-input"
                     id=" customCheckLogin"
@@ -175,33 +173,28 @@ class Login extends React.Component {
                     <span className="text-muted">Remember me</span>
                   </label>
                 </div> */}
-								<div className="text-center">
-									<Button
-										className="my-4"
-										color="primary"
-										type="submit"
-										disabled={loading}
-									>
-										{loading ? (
-											<div>
-												<Spinner
-													as="span"
-													animation="grow"
-													size="sm"
-													role="status"
-													aria-hidden="true"
-												/>{' '}
-												Signing in...
-											</div>
-										) : (
-											'Sign in'
-										)}
-									</Button>
-								</div>
-							</Form>
-						</CardBody>
-					</Card>
-					{/* <Row className="mt-3">
+                <div className="text-center">
+                  <Button className="my-4" color="primary" type="submit" disabled={loading}>
+                    {loading ? (
+                      <div>
+                        <Spinner
+                          as="span"
+                          animation="grow"
+                          size="sm"
+                          role="status"
+                          aria-hidden="true"
+                        />{' '}
+                        Signing in...
+                      </div>
+                    ) : (
+                      'Sign in'
+                    )}
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+          {/* <Row className="mt-3">
             <Col xs="6">
               <a
                 className="text-light"
@@ -221,10 +214,10 @@ class Login extends React.Component {
               </a>
             </Col>
           </Row> */}
-				</Col>
-			</React.Fragment>
-		);
-	}
+        </Col>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
